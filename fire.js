c8e40c715a106afbf616dfc2b7e129d33b763433
natan0j <mcbizuta@gmail.com>
2023-02-06 20:09:00 +0100

    commit on "Marshmallows"

--- fire.js
+++ fire.js
@@ -29,31 +29,28 @@ function colourChanger(){
         var i = document.getElementById("background");
         i.classList.add("backgroundblack");   
         var x = document.getElementById("fireplace");
-        if (x.style.display === "block") {
-                x.classList.add('hide');
-              } else {
                 x.style.display = "block";
-              }
         var y = document.getElementById("logs");
-        if (y.style.display === "block") {
-                y.style.display = "none";
-              } else {
                 y.style.display = "block";
-              }
         var z = document.getElementById("text");
-        if (z.style.display === "none") {
-                z.classList.add('show');
-              } else {
                 z.style.display = "none";
-              }
         var title = document.getElementById("info");
-        if (title.style.display === "block") {
-          title.classList.add('hide');
-        } else {
           title.style.display = "block";
-        }
+        var stick1 = document.getElementById("stick1");
+        stick1.style.display = "block";   
+        var stick2 = document.getElementById("stick2");
+        stick2.style.display = "block"; 
+        var stick3 = document.getElementById("stick3");
+        stick3.style.display = "block";   
 }
-function photoTransition(){
-  var photo = document.getElementsByClassName('photo');
-  console.log(photo)
+function showPage(x){
+    var about = document.getElementById(x);
+
+    if(about.style.display==="block"){
+    about.style.display = "none";
+    }
+    else {
+      about.style.display = "block"
+    }
+    about.classList.add('textIn');
 }
